3f97a5779e1143a3798855aff0529d72191b3b98
邓鹏宇 <dengpy@rayeye.cn>
2020-11-26 13:55:57 +0800

    feat: add react router feature

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+
+
+import React, { FC } from 'react';
+import { Button } from 'antd';
+
+const Home: FC = () => (
+  <div>
+    <Button type="primary">Button111</Button>
+  </div>
+);
+
+export default Home;
